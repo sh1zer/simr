1b4640beeea1e72780e83edb6a311dbacce10760
sh1zer <thatfellashizer@gmail.com>
2025-04-24 23:59:14 +0200

    added run_executable

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+use std::io::{Error, ErrorKind};
+
 fn handle_flag(flag: String) -> Option<String>{
     match flag.as_str(){
         "-help" => Some("Help coming soon!".to_string()),
         _ => None, 
+    }
+}
+
+fn run_executable(path: String, args: Vec<String>) -> Result<String, std::io::Error>{
+    if path.ends_with(".py"){
+        let status = std::process::Command::new("python3").args([path].into_iter().chain(args.into_iter())).status()?;
+        if status.success(){
+            return Ok("Hip hip hurray".to_string());
+        }
+        else{
+            return Err(Error::new(ErrorKind::Other, "Call ended with status {status}"));
+        }
+    }
+    let status = std::process::Command::new(path).args(args).status()?;
+    if status.success(){
+        Ok("Hip hip hurray".to_string())
+    }
+    else{
+        Err(Error::new(ErrorKind::Other, "Call ended with status {status}"))
     }
 }
 fn main() {
@@ -14,11 +35,16 @@ fn main() {
     let args = std::env::args().skip(1);
     for arg in args{
         println!("#handling {arg}");
+
         if arg.starts_with('-'){
             match handle_flag(arg){
                 Some(ret) => println!("{ret}"),
                 None => println!("Invalid flag"),
             }
+        }
+
+        else{
+            let _ = run_executable(arg, [].to_vec());
         }
     }
 }
